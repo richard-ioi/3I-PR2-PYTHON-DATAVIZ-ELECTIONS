8e2b8ac079fc5ce0b096b337e8cf742ef43585c8
Emily Renard <emily.renard@edu.esiee.fr>
2020-12-28 18:11:19 +0100

    ajout des derniers csv pour les annees 2002, 2007, 2017, ne pas preter attention au contenu de app2.py

--- app2.py
+++ app2.py
@@ -2,61 +2,111 @@
 
 # Run this app with `python app.py` and
 # visit localhost:8051/ in your web browser.
-from readFiles import departmentQuery
+
+############ imports ###########
+#from readFiles import departmentQuery
+from readFiles import *
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+import pygal
+from statistics import mean
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
 ###### reading csv data files ############
-#Data from the first round of the 2012 french presidential elections
-d121 = pd.read_csv('data/resultats_communes_T1_2012.csv' ,error_bad_lines=False, sep=';', low_memory=False)
-#Data from the first round of the 1995 french presidential elections (row 0 to 36671)
+#Data from the first and second rounds of the 1995 french presidential elections (round 1 : row 0 to 36671, round 2 : row X to Y)
 d951 = pd.read_csv('data/elections_1995_par_ville.csv', nrows=36671, low_memory=False)
 d952 = pd.read_csv('data/elections_1995_par_ville.csv', skiprows=36671, low_memory=False)
+#Data from the first and second rounds of the 2002 french presidential elections 
+d021 = pd.read_csv('data/elections_2002_T1.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+d022 = pd.read_csv('data/election_2002_T2.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+#Data from the first and second rounds of the 2007 french presidential elections 
+d071 = pd.read_csv('data/elections_2007_T1.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+d072 = pd.read_csv('data/elections_2007_T2.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+#Data from the first round of the 2012 french presidential elections
+d121 = pd.read_csv('data/resultats_communes_T1_2012.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+d122 = pd.read_csv('data/resultats_communes_T2.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+#Data from the first and second rounds of the 2017 french presidential elections 
+d171 = pd.read_csv('data/election_2017_T1.csv' ,error_bad_lines=False, sep=';', low_memory=False)
+d172 = pd.read_csv('data/election_2017_T2.csv' ,error_bad_lines=False, sep=';', low_memory=False)
 #removes disruptive characters from column names of csv files, lowers all characters
-d121.columns = d121.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('-', '_')
-d951.columns = d951.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('-', '_')
+d121.columns = normaliseNames(d121)
+d951.columns = normaliseNames(d951)
+seinestdenis121 = departmentQuery('78', d121) #d121.query("code_du_département == '93'")
 
-seinestdenis121 = departmentQuery('92', d121) #d121.query("code_du_département == '93'")
-
-fig = px.scatter(seinestdenis121, x=seinestdenis121['inscrits'], y=seinestdenis121['%_vot/ins'], hover_name="libellé_de_la_commune", height=300, title="2012")
-fig.update_layout(
-    margin=dict(l=20, r=20, t=30, b=20),
-    paper_bgcolor="LightSteelBlue",
-)
-
-app = dash.Dash(__name__, title="Analyse des données d'élections")#, external_stylesheets=external_stylesheets)
-app.layout = html.Div([
-    html.H1(children='Elections présidentielles françaises depuis 1995'),
+#Year dataframe
+# initialize list of lists 
+year_data = [['1995', d951], ['2002', d021], ['2007', d071], ['2012',d121], ['2017', d171]] 
   
-    html.P(children='''
-        Dash: A web application framework for Python.
-    '''),
-    
-    html.Div(
-        className='drop-down-year',
-        children=[ 
-            dcc.Dropdown(
-                options=[
-                    {'label': '1995', 'value': '1995'},
-                    {'label': '2002', 'value': '2002'},
-                    {'label': '2007', 'value': '2007'},
-                    {'label':'2012', 'value':'2012'},
-                    {'label':'2017', 'value':'2017'}
-                ],
-                placeholder="Sélectionnez une année"
-            ),
-            dcc.Graph(
-                id='life-exp-vs-gdp',
-                figure=fig
-            )
-        ]
-    )
-])
+# Create the pandas DataFrame 
+df = pd.DataFrame(year_data, columns = ['year', 'associated_df']) 
+  
+# print dataframe. 
+df
 
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8051 )
+# fig = px.scatter(seinestdenis121, x=seinestdenis121['inscrits'], y=seinestdenis121['votants'], hover_name="libellé_de_la_commune", height=300, title="2012")
+# fig.update_layout(
+#     margin=dict(l=20, r=20, t=30, b=20),
+#     paper_bgcolor="LightSteelBlue",
+# )
+# ############# map drawing ##########
+# # mapEurope = go.Figure(go.Scattergeo())
+# # mapEurope.update_geos(
+# #     visible=True, resolution=110, scope="europe",
+# #     fitbounds="locations",
+# #     showcountries=True, countrycolor="Black",
+# #     showsubunits=True, subunitcolor="Blue"
+# # )
+# # mapEurope.update_layout(height=300, margin={"r":0,"t":0,"l":0,"b":0})
+# # mapEurope.show()
+
+# ###### Moyenne des voix 2012###########
+# joly = d121['%_voix/ins_joly']
+
+# voixJoly = [voix for voix in joly]
+# voixJoly = commaToDot(voixJoly)
+# #voixJoly = stringToFloat(voixJoly)
+# print(voixJoly[:15])
+# floatJoly=[]
+# for i in range (1, len(voixJoly)):
+#     i = float(i)
+#     floatJoly.append(i)
+# moyenneJoly = mean(floatJoly)
+# print(moyenneJoly)
+# ############## dash app ############
+# app = dash.Dash(__name__, title="Analyse des données d'élections")#, external_stylesheets=external_stylesheets)
+# app.layout = html.Div([
+#     html.H1(children='Elections présidentielles françaises depuis 1995'),
+  
+#     html.P(children='''
+#         Dash: A web application framework for Python.
+#     '''),
+    
+#     html.Div(
+#         className='drop-down-year',
+#         children=[ 
+#             dcc.Dropdown(
+#                 options=[
+#                     {'label': '1995', 'value': '1995'},
+#                     {'label': '2002', 'value': '2002'},
+#                     {'label': '2007', 'value': '2007'},
+#                     {'label':'2012', 'value':'2012'},
+#                     {'label':'2017', 'value':'2017'}
+#                 ],
+#                 placeholder="Sélectionnez une année"
+#             ),
+#             dcc.Graph(
+#                 id='life-exp-vs-gdp',
+#                 figure=fig
+#             )
+#         ]
+#     )
+# ])
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True, port=8051 )
--- readFiles.py
+++ readFiles.py
@@ -1,13 +1,27 @@
 import pandas as pd
-
+import pygal
 
 #removes disruptive characters from column names of csv files, lowers all characters
 def normaliseNames(name):
     return name.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '').str.replace('-', '_')
+#enables string to float convertion
+def commaToDot(name):
+    convert = []
+    for comma in name:
+        temp = comma.replace(',', '.')
+        convert.append(temp)   
+    return convert
+#Converts string in list to float
+# def stringToFloat(name):
+#     convert = []
+#     for string in name:
+#        # temp = float(string)
+#         #convert.append(temp)
+#     return convert
+
 #selects a specific department 
 def departmentQuery(code, name):
-    print(createQueryString(code))
-    return name.query(createQueryString(code))#"code_du_département == '93'")
-
+    return name.query(createQueryString(code))
+#creates a string for queries
 def createQueryString(code):
     return f'code_du_département == "{code}"'

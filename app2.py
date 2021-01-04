126f23e1e59f6a6cac7092a065c7c9e0f14d5cba
Richard FOUQUOIRE <richard.fouquoire@edu.esiee.fr>
2021-01-05 00:00:59 +0100

    Modification du calcul de dTaux, désormais possible de l'obtenir en fonction de l'année voulue

--- app2.py
+++ app2.py
@@ -18,6 +18,45 @@ from readFiles import normaliseNames
 from readFiles import commaToDot
 from readFiles import departmentQuery
 from readFiles import createQueryString
+#pd.options.mode.chained_assignment = None  # default='warn'
+
+#Department and region names
+department_names = {
+    '1': 'Ain', '2': 'Aisne', '3': 'Allier', '4': 'Alpes-de-Haute-Provence', '5': 'Hautes-Alpes','6': 'Alpes-Maritimes', 
+    '7': 'Ardèche', '8': 'Ardennes', '9': 'Ariège', '10': 'Aube', '11': 'Aude','12': 'Aveyron', '13': 'Bouches-du-Rhône', 
+    '14': 'Calvados', '15': 'Cantal', '16': 'Charente', '17': 'Charente-Maritime', '18': 'Cher', '19': 'Corrèze', '2A': 'Corse-du-Sud', 
+    '2B': 'Haute-Corse', '21': 'Côte-d\'Or', '22': 'Côtes-d\'Armor', '23': 'Creuse', '24': 'Dordogne', '25': 'Doubs', '26': 'Drôme',
+    '27': 'Eure', '28': 'Eure-et-Loir', '29': 'Finistère', '30': 'Gard', '31': 'Haute-Garonne', '32': 'Gers','33': 'Gironde', '34': 'Hérault', 
+    '35': 'Ille-et-Vilaine', '36': 'Indre', '37': 'Indre-et-Loire','38': 'Isère', '39': 'Jura', '40': 'Landes', '41': 'Loir-et-Cher', 
+    '42': 'Loire', '43': 'Haute-Loire','44': 'Loire-Atlantique', '45': 'Loiret', '46': 'Lot', '47': 'Lot-et-Garonne', '48': 'Lozère',
+    '49': 'Maine-et-Loire', '50': 'Manche', '51': 'Marne', '52': 'Haute-Marne', '53': 'Mayenne','54': 'Meurthe-et-Moselle', '55': 'Meuse', 
+    '56': 'Morbihan', '57': 'Moselle', '58': 'Nièvre', '59': 'Nord','60': 'Oise', '61': 'Orne', '62': 'Pas-de-Calais', '63': 'Puy-de-Dôme', 
+    '64': 'Pyrénées-Atlantiques','65': 'Hautes-Pyrénées', '66': 'Pyrénées-Orientales', '67': 'Bas-Rhin', '68': 'Haut-Rhin', '69': 'Rhône',
+    '70': 'Haute-Saône', '71': 'Saône-et-Loire', '72': 'Sarthe', '73': 'Savoie', '74': 'Haute-Savoie','75': 'Paris', '76': 'Seine-Maritime', 
+    '77': 'Seine-et-Marne', '78': 'Yvelines', '79': 'Deux-Sèvres','80': 'Somme', '81': 'Tarn', '82': 'Tarn-et-Garonne', 
+    '83': 'Var', '84': 'Vaucluse', '85': 'Vendée','86': 'Vienne', '87': 'Haute-Vienne', '88': 'Vosges', '89': 'Yonne', '90': 'Territoire de Belfort',
+    '91': 'Essonne', '92': 'Hauts-de-Seine', '93': 'Seine-Saint-Denis', '94': 'Val-de-Marne', '95': 'Val-d\'Oise',
+}
+
+region_names = {
+    'Auvergne-Rhône-Alpes': ['01', '03', '07', '15', '26', '38', '42', '43', '63', '69', '73', '74'],
+    'Bourgogne-Franche-Comté': ['21', '25', '39', '58', '70', '71', '89', '90'],
+    'Bretagne': ['35', '22', '56', '29'],
+    'Centre-Val de Loire': ['18', '28', '36', '37', '41', '45'],
+    'Corse': ['2A', '2B'],
+    'Grand Est': ['08', '10', '51', '52', '54', '55', '57', '67', '68', '88'],
+    'Guadeloupe': ['971'],
+    'Guyane': ['973'],
+    'Hauts-de-France': ['02', '59', '60', '62', '80'],
+    'Île-de-France': ['75', '77', '78', '91', '92', '93', '94', '95'],
+    'La Réunion': ['974'],
+    'Martinique': ['972'],
+    'Normandie': ['14', '27', '50', '61', '76'],
+    'Nouvelle-Aquitaine': ['16', '17', '19', '23', '24', '33', '40', '47', '64', '79', '86', '87'],
+    'Occitanie': ['09', '11', '12', '30', '31', '32', '34', '46', '48', '65', '66', '81', '82'],
+    'Pays de la Loire': ['44', '49', '53', '72', '85'],
+    'Provence-Alpes-Côte d\'Azur': ['04', '05', '06', '13', '83', '84'],
+}
 
 ###### importing csv data files ############
 url1995=requests.get("https://perso.esiee.fr/~fouquoir/E3/Python_Projet/data/election_1995_par_ville.csv").content
@@ -74,23 +113,50 @@ d071 = d071.assign(year = "2007")
 d121 = d121.assign(year = "2012")
 d171 = d171.assign(year = "2017")
 
+def calcul_taux_participation_departement(dYear):
+    dT={}
+    vCompteur=0
+    for i in department_names.keys():
+        vCompteur+=1
+        vD=departmentQuery(i,dYear)
+        moyenne=0
+        vS=''
+        for j in (vD['%_vot/ins']):
+            vS=j
+            vS=vS.replace(',','.')
+            moyenne+=float(vS)
 
+        moyenne=moyenne/vD['%_vot/ins'].size
+
+        if (vCompteur<10):
+            i='0'+i
+        dT[i]=[moyenne]
+
+    dTaux=pd.DataFrame.from_dict(dT,orient='index',columns=['taux_de_participation'])
+    dTaux=dTaux.reset_index()
+    dTaux=dTaux.rename(columns={'index':'code'})
+    return dTaux
 ############# map drawing ##########
-print("Load de la map...")
-with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as response:
-    departements = json.load(response)
-dTaux = pd.read_csv("https://perso.esiee.fr/~fouquoir/E3/Python_Projet/data/taux.csv",
-                   dtype={"taux_de_participation": float})
-print("Traçage de la map...")
-fig = px.choropleth_mapbox(dTaux, geojson=departements, color="taux_de_participation",
-                    locations="code", featureidkey="properties.code",
-                    center={"lat": 47.5, "lon": 2.3},
-                    mapbox_style="carto-positron", zoom=4.5
-                   )
-fig.update_geos(fitbounds="locations", visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-#fig.show()
-print("Map finie")
+def draw_map(dYear):
+    print("Load de la map...")
+    with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as response:
+        departements = json.load(response)
+    dTauxFinal=calcul_taux_participation_departement(dYear)
+    print("Traçage de la map...")
+    global map
+    map = px.choropleth_mapbox(dTauxFinal, geojson=departements, color="taux_de_participation",
+                        locations="code", featureidkey="properties.code",
+                        center={"lat": 47.5, "lon": 2.6},
+                        mapbox_style="carto-positron", zoom=4.4
+                    )
+    map.update_geos(fitbounds="locations", visible=False)
+    map.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                    width=800, height=400)
+    #map.show()
+    print("Map finie")
+
+draw_map(d171)
+
 ###### Moyenne des voix 2012###########
 # joly = d121['%_voix/ins_joly']
 
@@ -104,45 +170,6 @@ print("Map finie")
 #     floatJoly.append(i)
 # moyenneJoly = mean(floatJoly)
 # print(moyenneJoly)
-
-
-#Department and region names
-department_names = {
-    '1': 'Ain', '2': 'Aisne', '3': 'Allier', '4': 'Alpes-de-Haute-Provence', '5': 'Hautes-Alpes','6': 'Alpes-Maritimes', 
-    '7': 'Ardèche', '8': 'Ardennes', '9': 'Ariège', '10': 'Aube', '11': 'Aude','12': 'Aveyron', '13': 'Bouches-du-Rhône', 
-    '14': 'Calvados', '15': 'Cantal', '16': 'Charente', '17': 'Charente-Maritime', '18': 'Cher', '19': 'Corrèze', '2A': 'Corse-du-Sud', 
-    '2B': 'Haute-Corse', '21': 'Côte-d\'Or', '22': 'Côtes-d\'Armor', '23': 'Creuse', '24': 'Dordogne', '25': 'Doubs', '26': 'Drôme',
-    '27': 'Eure', '28': 'Eure-et-Loir', '29': 'Finistère', '30': 'Gard', '31': 'Haute-Garonne', '32': 'Gers','33': 'Gironde', '34': 'Hérault', 
-    '35': 'Ille-et-Vilaine', '36': 'Indre', '37': 'Indre-et-Loire','38': 'Isère', '39': 'Jura', '40': 'Landes', '41': 'Loir-et-Cher', 
-    '42': 'Loire', '43': 'Haute-Loire','44': 'Loire-Atlantique', '45': 'Loiret', '46': 'Lot', '47': 'Lot-et-Garonne', '48': 'Lozère',
-    '49': 'Maine-et-Loire', '50': 'Manche', '51': 'Marne', '52': 'Haute-Marne', '53': 'Mayenne','54': 'Meurthe-et-Moselle', '55': 'Meuse', 
-    '56': 'Morbihan', '57': 'Moselle', '58': 'Nièvre', '59': 'Nord','60': 'Oise', '61': 'Orne', '62': 'Pas-de-Calais', '63': 'Puy-de-Dôme', 
-    '64': 'Pyrénées-Atlantiques','65': 'Hautes-Pyrénées', '66': 'Pyrénées-Orientales', '67': 'Bas-Rhin', '68': 'Haut-Rhin', '69': 'Rhône',
-    '70': 'Haute-Saône', '71': 'Saône-et-Loire', '72': 'Sarthe', '73': 'Savoie', '74': 'Haute-Savoie','75': 'Paris', '76': 'Seine-Maritime', 
-    '77': 'Seine-et-Marne', '78': 'Yvelines', '79': 'Deux-Sèvres','80': 'Somme', '81': 'Tarn', '82': 'Tarn-et-Garonne', 
-    '83': 'Var', '84': 'Vaucluse', '85': 'Vendée','86': 'Vienne', '87': 'Haute-Vienne', '88': 'Vosges', '89': 'Yonne', '90': 'Territoire de Belfort',
-    '91': 'Essonne', '92': 'Hauts-de-Seine', '93': 'Seine-Saint-Denis', '94': 'Val-de-Marne', '95': 'Val-d\'Oise',
-}
-
-region_names = {
-    'Auvergne-Rhône-Alpes': ['01', '03', '07', '15', '26', '38', '42', '43', '63', '69', '73', '74'],
-    'Bourgogne-Franche-Comté': ['21', '25', '39', '58', '70', '71', '89', '90'],
-    'Bretagne': ['35', '22', '56', '29'],
-    'Centre-Val de Loire': ['18', '28', '36', '37', '41', '45'],
-    'Corse': ['2A', '2B'],
-    'Grand Est': ['08', '10', '51', '52', '54', '55', '57', '67', '68', '88'],
-    'Guadeloupe': ['971'],
-    'Guyane': ['973'],
-    'Hauts-de-France': ['02', '59', '60', '62', '80'],
-    'Île-de-France': ['75', '77', '78', '91', '92', '93', '94', '95'],
-    'La Réunion': ['974'],
-    'Martinique': ['972'],
-    'Normandie': ['14', '27', '50', '61', '76'],
-    'Nouvelle-Aquitaine': ['16', '17', '19', '23', '24', '33', '40', '47', '64', '79', '86', '87'],
-    'Occitanie': ['09', '11', '12', '30', '31', '32', '34', '46', '48', '65', '66', '81', '82'],
-    'Pays de la Loire': ['44', '49', '53', '72', '85'],
-    'Provence-Alpes-Côte d\'Azur': ['04', '05', '06', '13', '83', '84'],
-}
 year_name = {
     1995 : [d951, d952],
     2002 : [d021, d022],
@@ -150,7 +177,6 @@ year_name = {
     2012 : [d121, d122],
     2017 : [d171, d172]
 }
-
 ############## dash app ############
 app = dash.Dash(__name__, title="Analyse des données d'élections",prevent_initial_callbacks=True)#, external_stylesheets=external_stylesheets)
 app.title = 'Elections présidentielles françaises de 1995 à 2017'
@@ -162,12 +188,12 @@ app.layout = html.Div([
     '''),
     html.Div(
         children=[
-            dcc.Graph(figure=fig)
+            #dcc.Graph(figure=fig)
         ]
     ),
     html.Div(
         className='drop-down-year',
-        children=[ 
+        children=[
             dcc.Dropdown(
                 id='departements',
                 options=[
@@ -191,6 +217,8 @@ app.layout = html.Div([
     ), 
     html.Div(
         children=[
+            dcc.Graph(figure=map), 
+            html.Br(),
             dcc.Graph(id='test-graph'),
             html.P("Selectionner l'année"),
             dcc.RangeSlider(
@@ -218,7 +246,7 @@ app.layout = html.Div([
 )
 def update_figure(selected_departement, selected_year, selected_round): 
     global filtered_df
-    print("Données prêtes")
+    draw_map(d171)
     for i,j  in year_name.items():
         print("i is  : "+str(i), "selected year is : "+str(selected_year))
         if(i == selected_year[0]):
@@ -226,6 +254,7 @@ def update_figure(selected_departement, selected_year, selected_round):
                 filtered_df = departmentQuery(selected_departement, j[0])
             elif(selected_round == 'T2'):
                 filtered_df = departmentQuery(selected_departement, j[1])
+            #map.update_layout(center={"lat": 47.5, "lon": 2.0}, zoom=7)
 
     fig = px.scatter(filtered_df, x='inscrits', y='votants', hover_name="libellé_de_la_commune", height=300, title=str(selected_year[0]))
     fig.update_layout(
